458708f980657c2722faf80351367a9600ac9f9c
Thomas Többe <toebbe.thomas@outlook.de>
2026-05-22 12:18:38 +0200

    feat: logo as home button in footer

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Logo } from '../../shared/components/logo/logo';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [RouterLink, Logo],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
