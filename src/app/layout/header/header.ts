0498c9684cd6cfe51ba362786d2d20fac3f520b7
Thomas Többe <toebbe.thomas@outlook.de>
2026-06-11 12:40:59 +0200

    style: two time s "new Survey" Button on a screen Fixes #28

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,5 +1,7 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, ChangeDetectionStrategy, computed, inject } from '@angular/core';
+import { Router, NavigationEnd, RouterLink } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter, map } from 'rxjs';
 import { Logo } from '../../shared/components/logo/logo';
 
 @Component({
@@ -9,4 +11,16 @@ import { Logo } from '../../shared/components/logo/logo';
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Header {}
+export class Header {
+  private readonly router = inject(Router);
+
+  private readonly currentUrl = toSignal(
+    this.router.events.pipe(
+      filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+      map((e) => e.urlAfterRedirects),
+    ),
+    { initialValue: this.router.url },
+  );
+
+  readonly showNewSurveyButton = computed(() => this.currentUrl().startsWith('/survey/'));
+}
